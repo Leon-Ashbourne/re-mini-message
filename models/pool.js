3a36ee17ea505140443de761025949fe790dff8f
Leon M. Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-26 18:27:35 +0530

    single exports is enough pool.js

--- models/pool.js
+++ models/pool.js
@@ -4,27 +4,10 @@ const dotenv = require("dotenv")
 
 dotenv.config();
 
-console.log(process.env.NODE_ENV);
-
-if(process.env.NODE_ENV === "production") {
-
-    module.exports = new Pool({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    })
-
-
-}else {
-
-    module.exports = new Pool({
-        host: "localhost",
-        database: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        port: process.env.DATABASE_PORT
-    })
-}
-
+module.exports = new Pool({
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
+})
